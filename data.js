344e93753e1b63150a609b8e4cd1ae19e83b83e3
Victor Sankar Ghosh <victorsankarghosh95@gmail.com>
2024-08-02 22:25:51 -0400

    Initial commit

---
+++ data.js
@@ -0,0 +1,205 @@
+data = {
+    "about": [
+        "With around 5 years of hands-on experience in data engineering and application development, I specialize in crafting efficient automation tools and enhancing data processes using a blend of Python, Java, and REST API integrations. Graduating with a Master's in Computer Science at Rutgers University, I blend academic insights with real-world applications to deliver solutions that enhance efficiency and drive innovation.",
+        " ",
+        "Professional Highlights::",
+        "\uD83E\uDDE9 FlexTrade Systems Inc.: Developed a Python library to streamline data manipulation on the SiSense platform and created robust automation tools for data ETL pipelines, significantly enhancing performance and scalability.",
+        "\uD83D\uDD0D Ericsson India Global Services Pvt. Ltd.: Optimized XML parsing scripts and automated daily tasks using Bash, improving data extraction efficiency by 40% and reducing processing times by 50%.",
+        "\uD83D\uDD28 CGI Information Systems Pvt. Ltd.: Enhanced legacy applications with new data templates and streamlined deployment processes, achieving major reductions in data analysis time and deployment durations.",
+        "\uD83C\uDF10 Laboratory for Computer Science Research: Partnered with researchers from Boston University and Gallaudet University, to develop a desktop application that improves accessibility for the deaf community through ASL video annotation.",
+        " ",
+        "Academic Achievements:",
+        "\uD83C\uDF93 Graduated with MS in Computer Science from Rutgers, the State University of New Jersey.",
+        "\uD83D\uDCCC Holder of multiple certifications from Oracle and Microsoft, with deep expertise in Java, Python, and database systems.",
+        " ",
+        "Awards and Recognition:",
+        "\u2B50 Recognized as an NPTEL Star for exceptional performance across multiple technical courses.",
+        "\uD83C\uDFC6 Awarded multiple Certificates of Appreciation for my dedication and impact in technological online education and community service.",
+        " ",
+        "Personal Philosophy and Future Goals:",
+        "\uD83D\uDCA1 Driven by a passion for technology and innovation, I thrive in environments that challenge the status quo and require forward-thinking solutions. My goal is to contribute to projects that leverage technology to improve lives and streamline operations.",
+        "\uD83D\uDD0D Continually seeking new challenges that will allow me to leverage and expand my skills in software development and data engineering. I am particularly interested in opportunities that focus on the development of intelligent systems and applications that address real-world problems."
+    ],
+    "education":[
+        {
+            "school": "Rutgers, The State University of New Jersey",
+            "location": "New Brunswick, New Jersey, USA",
+            "from": "September 2022",
+            "to": "May 2024",
+            "degree": "Master of Science",
+            "major": "Computer Science",
+            "minor": "",
+            "logo": "rutgers.png",
+            "grade": "GPA: 3.67/4",
+            "desc": [
+                "Data Structures and Algorithms", "Operating Systems Theory and Design", "Database Management Systems Engineering for Data Science",
+                "Introduction to Artificial Intelligence", "Programming Languages and Compilers", "Software Engineering",
+                "Machine Learning", "Computational Biomedicine"
+            ],
+        },{
+            "school": "Techno India, Salt Lake",
+            "location": "Kolkata, West Bengal, India",
+            "from": "August 2013",
+            "to": "June 2017",
+            "degree": "Bachelor of Technology",
+            "major": "Computer Science and Engineering",
+            "minor": "",
+            "logo": "techno.png",
+            "grade": "GPA: 8.27/10",
+        },{
+            "school": "Don Bosco School, Park Circus",
+            "location": "Kolkata, West Bengal, India",
+            "from": "April 2009",
+            "to": "March 2013",
+            "degree": "Higher Secondary Education",
+            "major": "Computer Science",
+            "minor": "",
+            "logo": "don_bosco.jpg",
+            "grade": "Percent: 93.8%",
+        }],
+    "work_exp": [
+        {
+            "company": "FlexTrade Systems Inc",
+            "role": "Trading Analyst Intern",
+            "location": "Great Neck, New York, United States of America",
+            "from": "May 2023",
+            "to": "August 2023",
+            "logo": "flextrade.png",
+            "desc": [
+                "Built a custom External Python Library to empower the development of REST API-based Automation Tools for the extraction and visualization of trading data within the SiSense Platform."
+            ],
+        },{
+            "company": "Laboratory for Computer Science Research",
+            "role": "Student Research Programmer",
+            "location": "Piscataway, New Jersey, United States of America",
+            "from": "February 2023",
+            "to": "May 2023",
+            "logo": "rutgers.png",
+            "desc": [
+                "Developed and enhanced a MacOS-specific GUI application to facilitate American Sign Language (ASL) video annotation on open-source project in collaboration with linguists and bio-medical researchers from Boston University and Gallaudet University",
+                "Performed root cause analysis of critical issues, thorough code review, employed integration testing to ensure adherence to coding standards and best practices"
+            ],
+        },{
+            "company": "FlexTrade Systems Inc",
+            "role": "Software Developer",
+            "location": "Pune, Maharashtra, India",
+            "from": "May 2022",
+            "to": "August 2022",
+            "logo": "flextrade.png",
+            "desc":[
+                "Implemented all phases of the SDLC for a suite of Automation Tools for ETL on data pipelines, initiating financial data backup and verification systems within the SiSense Platform, hosted on AWS, leveraging REST-API integration"
+
+            ],
+        },{
+            "company": "Ericsson India Global Services Pvt. Ltd.",
+            "role": "Associate Software Engineer",
+            "location": "Kolkata, West Bengal, India",
+            "from": "March 2020",
+            "to": "September 2021",
+            "logo": "ericsson.png",
+            "desc": [
+                "Integrated Python & Bash shell backend scripts for XML parsing of SOAP messages using XPath, improving batch data extraction efficiency by 40% and automating daily processing tasks",
+                "Collaborated with cross-functional deployment teams to integrate CI/CD processes, using Docker (containerization) and Kubernetes (orchestration), achieving efficient deployment and management of microservices."
+            ],
+        },{
+            "company": "CGI Information Systems and Management Consultants Pvt. Ltd.",
+            "role": "Associate Software Engineer",
+            "location": "Bangalore, Karnataka, India",
+            "from": "August 2017",
+            "to": "August 2019",
+            "logo": "cgi.jpeg",
+            "desc": [
+                "Employed test driven development to enable responsive design in a legacy Desktop-app built in Java (Swings) & Python",
+                "Provided production support and streamlined deployments on RedHat OpenShift Platform, reducing deployment time by 30% and increasing overall efficiency"
+            ]
+        }
+        ],
+    "credentials": [
+        {
+            "title": "Microsoft Technology Associate",
+            "from": "March 2022",
+            "to": "April 2022",
+            "desc":[
+                "Introduction to Programming Using Java",
+                "Introduction to Programming Using Python",
+                "Introduction to Programming Using HTML & CSS",
+                "Introduction to Programming Using Javascript",
+                "Database Fundamentals",
+                "Software Development Fundamentals"
+            ]
+        },{
+            "title": "Oracle Certified Associate",
+            "from": "January 2022",
+            "to": "January 2022",
+            "desc":[
+                "Java Certified Foundations Associate"
+            ]
+        },{
+            "title": "National Programme on Technology Enhanced Learning (NPTEL)",
+            "from": "July 2016",
+            "to": "December 2020",
+            "desc":[
+                "Privacy & Security in Online Social-Media",
+                "Programming, Data Structures and Algorithms Using Python",
+                "Programming in Java",
+                "Programming in C++",
+                "Cloud Computing",
+                "Foundations of Computer System Design",
+                "Introduction to Internet of Things",
+                "Real-Time Operating Systems",
+                "Discrete Mathematics",
+                "Computer Networks and Internet Protocols",
+                "Fundamentals of Database Systems",
+                "Software Engineering",
+                "Big Data Computing",
+                "Switching Circuits and Logic Design",
+                "Introduction to Haskell Programming",
+                "Computer Graphics",
+                "Introduction to Algorithms and their Analysis",
+                "Introduction to Programming in C",
+                "Introduction to Operating Systems",
+                "Compiler Design",
+                "Computer Organization and Architecture: A Pedagogical Aspect",
+                "Introduction to Automata, Languages and Computation"
+            ]
+        },{
+            "title": "HackerRank",
+            "from": "January 2022",
+            "to": "March 2024",
+            "desc":[
+                "C# (Basic)",
+                "Java (Basic)",
+                "Python (Basic)",
+                "JavaScript (Basic)",
+                "Go (Basic)",
+                "Problem Solving (Basic + Intermediate)",
+                "SQL (Basic + Intermediate)",
+                "Software Engineer Intern",
+                "Software Engineer"
+            ]
+        },{
+            "title": "CutShort",
+            "from": "February 2022",
+            "to": "March 2022",
+            "desc":[
+                "C",
+                "C#",
+                "C++",
+                "HTML/CSS",
+                "Java",
+                "Python",
+                "Data Structures",
+                "Algorithms"
+            ]
+        }
+    ],
+    "bg_icon": [
+        "cpp.png", "python.png", "java.png",
+        "js.png", "html.png", "css.png", "bootstrap.png",
+        "react.png", "mongodb.png", "angular.png", "ts.png",
+        "mysql.png", "android.png",
+        "linux.png", "git.png",
+        "db.png",  "api.png",
+        "flask.png",
+    ],
+}
---
+++ populate.js
@@ -0,0 +1,113 @@
+var icon_row ="<div>";
+for(var rep=0; rep<2; rep++){
+    for (let i = 0; i < data.bg_icon.length; i++)
+        icon_row += '<img src="icons/bg_icons/' + data.bg_icon[i] + '">';
+}
+icon_row += "</div>";
+$('.bg_row').html(icon_row + icon_row);
+
+var row = $('.bg_section').html(), bg = "";
+for(var i=0; i<24; i++) bg += row;
+$('.bg_section').html(bg);
+
+
+data.about.forEach(function (item,idx){
+    $('#about_p').append(item).append('<br>');
+})
+
+
+data.education.forEach(function (item, idx){
+    var logo = '<div class="card_logo_box">';
+    logo += '<div class="card_logo">';
+    logo += '<img src="icons/logos/' + item.logo + '">';
+    logo += '</div>';
+    logo += '</div>';
+    
+    
+
+    var content = '<div class="timeline_item">';
+    content += '<div class="card_info">';
+    if(idx%2===0) content += logo;
+    content += '<div class="card_date">' + item.from + " - " + item.to + '</div>';
+    if(idx%2!==0) content += logo;
+    content += '</div>';
+
+    content += '<div class="card_item">';
+    content += '<div class="card_title">' + item.school + '</div>';
+    content += '<div class="card_subtitle">' + item.location + '</div>';
+    content += '<div class="card_role">' + item.degree + ", " + item.major + ' (' + item.grade + ')' + '</div>';
+    content += '<div class="inner_card_date">' + item.from + " - " + item.to + '</div>';
+
+    if('desc' in item){
+        var ls = '<ul>';
+        item.desc.forEach(function (item){
+            ls += '<li>' + item + '</li>';
+        });
+        ls += '</ul>';
+        content += '<div class="card_content">' + ls + '</div>';
+    }
+
+    content += '</div>';
+    content += '</div>';
+
+    $('#education').find('.timeline_list').append(content);
+});
+
+data.work_exp.forEach(function (item, idx){
+    var logo = '<div class="card_logo_box">';
+    logo += '<div class="card_logo">';
+    logo += '<img src="icons/logos/' + item.logo + '">';
+    logo += '</div>';
+    logo += '</div>';
+
+    var ls = '<ul>';
+    item.desc.forEach(function (item){ ls += '<li>' + item + '</li>'; });
+    ls += '</ul>';
+
+    var content = '<div class="timeline_item">';
+    content += '<div class="card_info">';
+    if(idx%2===0) content += logo;
+    content += '<div class="card_date">' + item.from + " - " + item.to + '</div>';
+    if(idx%2!==0) content += logo;
+    content += '</div>';
+
+    content += '<div class="card_item">';
+    content += '<div class="card_title">' + item.company + '</div>';
+    content += '<div class="card_subtitle">' + item.location + '</div>';
+    content += '<div class="card_role">' + item.role + '</div>';
+    content += '<div class="inner_card_date">' + item.from + " - " + item.to + '</div>';
+    content += '<div class="card_content">' + ls + '</div>';
+    content += '</div>';
+    content += '</div>';
+
+    $('#work_exp').find('.timeline_list').append(content);
+});
+
+data.credentials.forEach(function (item, idx){
+    var logo = '<div class="card_logo_box">';
+    logo += '<div class="card_logo">';
+    // logo += '<img src="icons/logos/' + item.logo + '">';
+    logo += idx + 1;
+    logo += '</div>';
+    logo += '</div>';
+
+    var ls = '<ul>';
+    item.desc.forEach(function (item){ ls += '<li>' + item + '</li>'; });
+    ls += '</ul>';
+
+    var content = '<div class="timeline_item">';
+    content += '<div class="card_info">';
+    if(idx%2===0) content += logo;
+    content += '<div class="card_date">' + item.from + " - " + item.to + '</div>';
+    if(idx%2!==0) content += logo;
+    content += '</div>';
+
+    content += '<div class="card_item">';
+    content += '<div class="card_title">' + item.title + '</div>';
+    content += '<div class="inner_card_date">' + item.from + " - " + item.to + '</div>';
+    content += '<div class="card_content">' + ls + '</div>';
+    content += '</div>';
+    content += '</div>';
+
+    $('#credentials').find('.timeline_list').append(content);
+}); 
